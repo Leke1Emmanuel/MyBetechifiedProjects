5d4e5dd37cbb208c2d5bebaeb520b0aedcac6b7b
Transcendent <adeyemigbemileke12@gmail.com>
2025-10-10 16:20:08 +0100

    Assignment 1

---
+++ intro.js
@@ -0,0 +1,5 @@
+const fullname ="Adeyemi Gbemileke Emmanuel"
+const age = 18
+const favproglang = "PYTHON!!!"
+
+console.log("I am",fullname,"and I'm",age,"yrs old and my fav programming lang is",favproglang)
